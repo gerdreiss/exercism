706c3a317c59983f73d8fe63df69a179afd012ea
Gerd Reiss <gerd@reiss.pro>
2021-08-24 21:41:09 +0200

    Protein Translation

---
+++ scala/protein-translation/src/main/scala/ProteinTranslation.scala
@@ -0,0 +1,29 @@
+object ProteinTranslation {
+
+  val rnaToProtein = Map(
+    "AUG" -> "Methionine",
+    "UUU" -> "Phenylalanine",
+    "UUC" -> "Phenylalanine",
+    "UUA" -> "Leucine",
+    "UUG" -> "Leucine",
+    "UCU" -> "Serine",
+    "UCC" -> "Serine",
+    "UCA" -> "Serine",
+    "UCG" -> "Serine",
+    "UAU" -> "Tyrosine",
+    "UAC" -> "Tyrosine",
+    "UGU" -> "Cysteine",
+    "UGC" -> "Cysteine",
+    "UGG" -> "Tryptophan",
+    "UAA" -> "STOP",
+    "UAG" -> "STOP",
+    "UGA" -> "STOP"
+  )
+
+  def proteins(rna: String): Seq[String] =
+    rna
+      .grouped(3)
+      .map(rnaToProtein)
+      .takeWhile(_ != "STOP")
+      .toList
+}
