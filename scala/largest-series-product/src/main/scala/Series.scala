595d7f1c74fd4dee7f82ed5d4f0d34df27e4f3b2
Gerd Reiss <gerd@reiss.pro>
2021-08-25 13:02:31 +0200

    Largest Series Product

--- scala/largest-series-product/src/main/scala/Series.scala
+++ scala/largest-series-product/src/main/scala/Series.scala
@@ -1,8 +1,6 @@
-import scala.util.matching.Regex
-
 object Series {
   def largestProduct(length: Int, source: String): Option[Int] =
-    if (length == 0) Some(1)
-    else if (length < 0 || length > source.length || !source.forall(_.isDigit)) None
+    if (length < 0 || length > source.length || !source.forall(_.isDigit)) None
+    else if (length == 0) Some(1)
     else Some(source.sliding(length).map(_.map(_.toString.toInt).product).max)
 }
