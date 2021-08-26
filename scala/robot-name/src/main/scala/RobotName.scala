65fbc1f80729b02bfa5291c94da5d8c99f68e6aa
Gerd Reiss <gerd@reiss.pro>
2021-08-26 20:51:01 +0200

    Robot Name

--- scala/robot-name/src/main/scala/RobotName.scala
+++ scala/robot-name/src/main/scala/RobotName.scala
@@ -1,26 +1,30 @@
 import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.util.Random
 
 class Robot(val letters: Int = 2, val digits: Int = 3) {
-  private val nameRegex = "([A-Z]{2}[0-9]{3})".r
-  private var cache     = List(generate)
 
-  @tailrec
-  private def generate: String = {
-    s"$randomChar$randomChar$randomInt$randomInt$randomInt" match {
-      case nameRegex(s) => s
-      case _            => generate
-    }
-  }
-  private def randomChar = Random.nextPrintableChar().toUpper
-  private def randomInt  = Random.nextInt(10)
+  private var _name = RobotNames.generate(letters, digits)
 
-  def name: String = cache.head
+  def name: String = _name
 
   def reset(): Unit = {
-    val newName = generate
-    if (!cache.contains(newName)) cache = newName :: cache
-    else reset()
+    _name = RobotNames.generate(letters, digits)
   }
+}
+
+object RobotNames {
+  private val nameRegex = "([A-Z]{2}[0-9]{3})".r
+  private val cache     = mutable.ListBuffer.empty[String]
+
+  @tailrec
+  def generate(letters: Int = 2, digits: Int = 3): String   =
+    randomName(letters, digits) match {
+      case nameRegex(s) if !cache.contains(s) => cache.append(s); s
+      case _                                  => generate(letters, digits)
+    }
+  private def randomName(letters: Int = 2, digits: Int = 3) =
+    (0 until letters).map(_ => Random.nextPrintableChar.toUpper).mkString +
+      (0 until digits).map(_ => Random.nextInt(10)).mkString
 
 }
