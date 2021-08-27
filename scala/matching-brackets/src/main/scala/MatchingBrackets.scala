76488a63f7c70f36d7661abf1a4fb09ea8d35892
Gerd Reiss <gerd@reiss.pro>
2021-08-27 08:59:54 +0200

    Matching Brackets

---
+++ scala/matching-brackets/src/main/scala/MatchingBrackets.scala
@@ -0,0 +1,18 @@
+import scala.annotation.tailrec
+
+object MatchingBrackets {
+
+  private val pairs = "[]{}()<>"
+
+  @tailrec
+  def recurse(input: String): Boolean =
+    pairs.sliding(2, 2).foldLeft(input)((remainder, br) => remainder.replace(br, "")) match {
+      case ""              => true
+      case r if r == input => false
+      case r               => recurse(r)
+    }
+
+  def isPaired(input: String): Boolean =
+    input.isEmpty || recurse(input.filter(c => pairs.contains(c)))
+
+}
