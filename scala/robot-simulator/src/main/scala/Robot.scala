742f814df5534977eade544d6dc6f82d3ce1080e
Gerd Reiss <gerd@reiss.pro>
2021-09-14 11:42:54 +0200

    Robot Simulator

--- scala/robot-simulator/src/main/scala/Robot.scala
+++ scala/robot-simulator/src/main/scala/Robot.scala
@@ -4,30 +4,30 @@ sealed trait Bearing {
 }
 object Bearing       {
   case object North extends Bearing {
-    override def turnLeft: Bearing  = Bearing.West
-    override def turnRight: Bearing = Bearing.East
-  }
-  case object East  extends Bearing {
-    override def turnLeft: Bearing  = Bearing.North
-    override def turnRight: Bearing = Bearing.South
+    override val turnLeft: Bearing  = Bearing.West
+    override val turnRight: Bearing = Bearing.East
   }
   case object South extends Bearing {
-    override def turnLeft: Bearing  = Bearing.East
-    override def turnRight: Bearing = Bearing.West
+    override val turnLeft: Bearing  = Bearing.East
+    override val turnRight: Bearing = Bearing.West
+  }
+  case object East  extends Bearing {
+    override val turnLeft: Bearing  = Bearing.North
+    override val turnRight: Bearing = Bearing.South
   }
   case object West  extends Bearing {
-    override def turnLeft: Bearing  = Bearing.South
-    override def turnRight: Bearing = Bearing.North
+    override val turnLeft: Bearing  = Bearing.South
+    override val turnRight: Bearing = Bearing.North
   }
 }
 
 case class Robot(bearing: Bearing, coordinates: (Int, Int)) {
-  def turnRight: Robot               = copy(bearing = bearing.turnRight)
   def turnLeft: Robot                = copy(bearing = bearing.turnLeft)
+  def turnRight: Robot               = copy(bearing = bearing.turnRight)
   def advance: Robot                 = this match {
     case Robot(Bearing.North, (x, y)) => copy(coordinates = (x, y + 1))
-    case Robot(Bearing.East, (x, y))  => copy(coordinates = (x + 1, y))
     case Robot(Bearing.South, (x, y)) => copy(coordinates = (x, y - 1))
+    case Robot(Bearing.East, (x, y))  => copy(coordinates = (x + 1, y))
     case Robot(Bearing.West, (x, y))  => copy(coordinates = (x - 1, y))
   }
   def simulate(steps: String): Robot = steps.foldLeft(this) { (r, ch) =>
