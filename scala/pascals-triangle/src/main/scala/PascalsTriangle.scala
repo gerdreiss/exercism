46f8c02ed5793b195047948843ad5ec5951fccf6
Gerd Reiss <gerd@reiss.pro>
2021-09-16 11:17:40 +0200

    Pascals Triangle - implementation

--- scala/pascals-triangle/src/main/scala/PascalsTriangle.scala
+++ scala/pascals-triangle/src/main/scala/PascalsTriangle.scala
@@ -1,13 +1,18 @@
 import scala.annotation.tailrec
 
 object PascalsTriangle {
-  def rows(rows: Int): List[List[Int]] = {
-    @tailrec
-    def helper(row: Int, acc: List[List[Int]]): List[List[Int]] =
-      if (row > rows) acc
-      else helper(row + 1, acc :+ ((1 :: acc.last.sliding(2).map(_.sum).toList) :+ 1))
 
-    if (rows < 3) (1 to rows).map(row => List.fill(row)(1)).toList
-    else helper(3, List(List(1), List(1, 1)))
-  }
+  def rows(rows: Int): List[List[Int]] =
+    // my original solution
+    {
+      @tailrec
+      def helper(row: Int, acc: List[List[Int]]): List[List[Int]] =
+        if (row > rows) acc
+        else helper(row + 1, acc :+ ((1 :: acc.last.sliding(2).map(_.sum).toList) :+ 1))
+
+      if (rows < 3) (1 to rows).map(row => List.fill(row)(1)).toList
+      else helper(3, List(List(1), List(1, 1)))
+    }
+    // a much more elegant solution:
+    // Stream.iterate(List(1))(prev => (0 +: prev) zip (prev :+ 0) map Function.tupled(_ + _)).take(rows).toList
 }
