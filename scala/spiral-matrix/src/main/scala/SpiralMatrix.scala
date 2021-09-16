f35b30028ca5af363e208f8bb5d34079c1b444b1
Gerd Reiss <gerd@reiss.pro>
2021-09-16 21:23:18 +0200

    Spiral Matrix - impl

--- scala/spiral-matrix/src/main/scala/SpiralMatrix.scala
+++ scala/spiral-matrix/src/main/scala/SpiralMatrix.scala
@@ -1,4 +1,9 @@
 object SpiralMatrix {
-  def spiralMatrix(n: Int): List[List[Int]] = ???
+  def spiralMatrix(side: Int): List[List[Int]] = {
+    def helper(n: Int, x: Int, y: Int): List[List[Int]] =
+      if (x == 0) List.empty
+      else (n until n + y).toList :: helper(n + y, y, x - 1).reverse.transpose
 
+    helper(1, side, side)
+  }
 }
