a8807f2c49168e441e945c67e33c170e401949a5
Gerd Reiss <gerd@reiss.pro>
2021-09-24 20:05:10 +0200

    Parallel Letter Frequency  - fixed group size

--- scala/parallel-letter-frequency/src/main/scala/Frequency.scala
+++ scala/parallel-letter-frequency/src/main/scala/Frequency.scala
@@ -1,10 +1,10 @@
 object Frequency {
 
   def frequency(numWorkers: Int, texts: Seq[String]): Map[Char, Int] = {
-    if (texts.isEmpty) Map.empty
+    if (numWorkers == 0 || texts.isEmpty) Map.empty
     else
       texts
-        .grouped(numWorkers)
+        .grouped(if (texts.length < numWorkers) 1 else texts.length / numWorkers)
         .toParArray
         .map(_.mkString.toLowerCase.filter(_.isLetter).groupBy(identity).mapValues(_.length))
         .reduce((x, y) => (x.keySet ++ y.keySet).map(ch => ch -> (x.getOrElse(ch, 0) + y.getOrElse(ch, 0))).toMap)
