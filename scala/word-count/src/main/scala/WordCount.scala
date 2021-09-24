d467551caaa24f5e37dd4d039776575ec0272b4c
Gerd Reiss <gerd@reiss.pro>
2021-09-24 19:24:53 +0200

    Word Count

--- scala/word-count/src/main/scala/WordCount.scala
+++ scala/word-count/src/main/scala/WordCount.scala
@@ -1,13 +1,14 @@
 case class WordCount(string: String) {
+
   lazy val countWords: Map[String, Int] =
     string.toLowerCase
       .split("[^a-z0-9']")
-      .map(s => if (isQuoted(s)) s.tail.init else s)
+      .map(unquote)
       .filterNot(_.isEmpty)
       .groupBy(identity)
       .mapValues(_.length)
 
+  private def unquote(s: String): String =
+    if (s.startsWith("\'") && s.endsWith("\'") || s.startsWith("\"") && s.endsWith("\"")) s.tail.init else s
 
-  private def isQuoted(s: String): Boolean =
-    s.startsWith("\'") && s.endsWith("\'") || s.startsWith("\"") && s.endsWith("\"")
 }
