f37e4571d8ef6287f7cb60046e0a1ef0410372a8
Gerd Reiss <gerd@reiss.pro>
2021-09-25 09:36:59 +0200

    Rna Transcription

--- scala/rna-transcription/src/main/scala/RnaTranscription.scala
+++ scala/rna-transcription/src/main/scala/RnaTranscription.scala
@@ -8,9 +8,17 @@ object RnaTranscription {
   )
 
   def toRna(dna: String): Option[String] =
-    dna.foldLeft(Option("")) {
-      case (Some(dna), nucleotide) => mapping.get(nucleotide).map(dna :+ _)
-      case _                       => None
+    dna.foldLeft(Option("")) { (maybeRna, nucleotide) =>
+      // solution 1
+      //dna.foldLeft(Option("")) {
+      //  case (Some(dna), nucleotide) => mapping.get(nucleotide).map(dna :+ _)
+      //  case _                       => None
+      //}
+      // solution 2, maybe a little more readable
+      for {
+        mapped <- mapping.get(nucleotide)
+        rna    <- maybeRna
+      } yield rna :+ mapped
     }
 
 }
