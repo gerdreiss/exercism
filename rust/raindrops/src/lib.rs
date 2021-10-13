d9c5a378e9fcbbe3f9e46edf226da0ea40eb2104
Gerd Reiss <gerd@reiss.pro>
2021-10-13 18:42:28 +0200

    minor improvements

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,9 +1,16 @@
 pub fn raindrops(n: u32) -> String {
-    let result = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
-        .iter()
-        .filter(|(k, _)| n % *k == 0)
-        .map(|(_, v)| v)
-        .fold(String::new(), |acc, v| format!("{}{}", acc, v));
+    let result = [
+        (3, "Pling".to_owned()),
+        (5, "Plang".to_owned()),
+        (7, "Plong".to_owned()),
+    ]
+    .iter()
+    .filter(|(k, _)| n % *k == 0)
+    .map(|(_, v)| v)
+    .fold(String::new(), |mut acc, v| {
+        acc.push_str(v);
+        acc
+    });
 
     if result.is_empty() {
         n.to_string()
