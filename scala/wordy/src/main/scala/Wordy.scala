8fe37985d8cbd5e89842c329290f07e329220cd8
Gerd Reiss <gerd@reiss.pro>
2021-10-15 15:26:07 +0200

    playing around with scalafmt

--- scala/wordy/src/main/scala/Wordy.scala
+++ scala/wordy/src/main/scala/Wordy.scala
@@ -7,22 +7,24 @@ object Wordy extends RegexParsers {
   val op: Parser[String] = "plus" | "minus" | "multiplied by" | "divided by" | "raised to the"
 
   val parser: Parser[Option[Int]] =
-    "What is" ~ number ~ rep(op ~ number ~ """((st|nd|rd|th) power)?""".r) ~ "?" ^^ { case _ ~ x ~ ops ~ _ =>
-      ops.foldLeft(Option[Int](x)) { case (Some(result), op ~ y ~ _) =>
-        op match {
-          case "plus"          => Some(result + y)
-          case "minus"         => Some(result - y)
-          case "multiplied by" => Some(result * y)
-          case "divided by"    => if (y == 0) None else Some(result / y)
-          case "raised to the" => Some(math.pow(result, y).toInt)
-          case _               => None
+    "What is" ~ number ~ rep(op ~ number ~ """((st|nd|rd|th) power)?""".r) ~ "?" ^^ {
+      case _ ~ x ~ ops ~ _ =>
+        ops.foldLeft(Option[Int](x)) {
+          case (Some(result), op ~ y ~ _) =>
+            op match {
+              case "plus" => Some(result + y)
+              case "minus" => Some(result - y)
+              case "multiplied by" => Some(result * y)
+              case "divided by" => if (y == 0) None else Some(result / y)
+              case "raised to the" => Some(math.pow(result, y).toInt)
+              case _ => None
+            }
         }
-      }
     }
 
   def answer(sentence: String): Option[Int] =
     parseAll(parser, sentence) match {
       case Success(result, _) => result
-      case _                  => None
+      case _ => None
     }
 }
--- scala/wordy/src/main/scala/WordyByErikSchierboom.scala
+++ scala/wordy/src/main/scala/WordyByErikSchierboom.scala
@@ -5,39 +5,40 @@ import scala.util.parsing.combinator._
 sealed trait Expression {
   def solve: Int
 }
-case class Operand(n: Int)                          extends Expression   {
+case class Operand(n: Int) extends Expression {
   def solve: Int = n
 }
-case class Sum(e1: Expression, e2: Expression)      extends Expression   {
+case class Sum(e1: Expression, e2: Expression) extends Expression {
   def solve: Int = e1.solve + e2.solve
 }
-case class Subtract(e1: Expression, e2: Expression) extends Expression   {
+case class Subtract(e1: Expression, e2: Expression) extends Expression {
   def solve: Int = e1.solve - e2.solve
 }
-case class Multiply(e1: Expression, e2: Expression) extends Expression   {
+case class Multiply(e1: Expression, e2: Expression) extends Expression {
   def solve: Int = e1.solve * e2.solve
 }
-case class Divide(e1: Expression, e2: Expression)   extends Expression   {
+case class Divide(e1: Expression, e2: Expression) extends Expression {
   def solve: Int = e1.solve / e2.solve
 }
-case class Power(e1: Expression, e2: Expression)    extends Expression   {
+case class Power(e1: Expression, e2: Expression) extends Expression {
   def solve: Int = Math.pow(e1.solve, e2.solve).toInt
 }
-object WordProblem                                  extends RegexParsers {
+object WordProblem extends RegexParsers {
 
-  def operand: Parser[Operand]       = """(0|-?[1-9]\d*)""".r ^^ { str => Operand(str.toInt) }
-  def operator: Parser[String]       = "plus" | "minus" | "multiplied by" | "divided by" | "raised to the"
+  def operand: Parser[Operand] = """(0|-?[1-9]\d*)""".r ^^ { str => Operand(str.toInt) }
+  def operator: Parser[String] = "plus" | "minus" | "multiplied by" | "divided by" | "raised to the"
   def expression: Parser[Expression] = operand | operation
 
   def operation: Parser[Expression] =
-    expression ~ (operator ~ expression).* ^^ { case expr ~ list =>
-      list.foldLeft(expr) {
-        case (left, "plus" ~ right)          => Sum(left, right)
-        case (left, "minus" ~ right)         => Subtract(left, right)
-        case (left, "multiplied by" ~ right) => Multiply(left, right)
-        case (left, "divided by" ~ right)    => Divide(left, right)
-        case (left, "raised to the" ~ right) => Power(left, right)
-      }
+    expression ~ (operator ~ expression).* ^^ {
+      case expr ~ list =>
+        list.foldLeft(expr) {
+          case (left, "plus" ~ right) => Sum(left, right)
+          case (left, "minus" ~ right) => Subtract(left, right)
+          case (left, "multiplied by" ~ right) => Multiply(left, right)
+          case (left, "divided by" ~ right) => Divide(left, right)
+          case (left, "raised to the" ~ right) => Power(left, right)
+        }
     }
 
   def equation: Parser[Expression] = // this last part below had to be fixed to make it work
@@ -46,7 +47,7 @@ object WordProblem                                  extends RegexParsers {
   def apply(input: String): Option[Int] = {
     parse(equation, input) match {
       case Success(expression, _) => Some(expression.solve)
-      case _                      => None
+      case _ => None
     }
   }
 }
