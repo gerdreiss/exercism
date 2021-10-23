a2bb786b974eaddd1f17cc2d14b18ff9cbaf7363
Gerd Reiss <gerd@reiss.pro>
2021-10-23 09:11:53 +0200

    Role-Playing Game

---
+++ rust/role-playing-game/src/lib.rs
@@ -0,0 +1,29 @@
+pub struct Player {
+    pub health: u32,
+    pub mana: Option<u32>,
+    pub level: u32,
+}
+
+impl Player {
+    pub fn revive(&self) -> Option<Player> {
+        if self.health > 0 { None } else {
+            Some(Player {
+                health: 100,
+                mana: if self.level >= 10 { Some(100) } else { None },
+                level: self.level,
+            })
+        }
+    }
+
+    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
+        if self.mana.is_none() {
+            self.health = self.health.checked_sub(mana_cost).unwrap_or_default();
+            0
+        } else if self.mana.unwrap() < mana_cost {
+            0
+        } else {
+            self.mana = self.mana.map(|m| m - mana_cost);
+            mana_cost * 2
+        }
+    }
+}
