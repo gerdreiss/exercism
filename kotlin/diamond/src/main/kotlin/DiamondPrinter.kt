6e0cde71a7246d338db41181d747b90c8ea20261
Gerd Reiss <gerd@reiss.pro>
2021-10-25 10:20:24 +0200

    Diamond

--- kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -1,10 +1,17 @@
 class DiamondPrinter {
     fun printToList(char: Char): List<String>? {
-        var chars = ('A'..char)
-        var idxs = 0..chars.count()
-        chars.foldIndexed(mutableListOf<String>()) { (idx, acc, c) ->
-            acc
-        }
-        return listOf()
+        val chars = ('A'..char)
+        val lines = chars.withIndex().map { printChar(it.value, it.index, chars.count()) }.toList()
+        return lines + lines.reversed().drop(1)
     }
+
+    private fun printChar(char: Char, index: Int, chars: Int): String =
+        if (index == 0) spaces(chars - 1) + "A" + spaces(chars - 1)
+        else spaces(chars - index - 1) +
+                char.toString() +
+                spaces(index + index - 1) +
+                char.toString() +
+                spaces(chars - index - 1)
+
+    private fun spaces(n: Int): String = " ".repeat(n)
 }
