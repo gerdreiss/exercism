47b78d84253806466073ce2a216b0918b8ea606c
Gerd Reiss <gerd@reiss.pro>
2021-10-26 08:10:01 +0200

    Anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashMap, HashSet};
 
+// my solution
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     possible_anagrams
         .iter()
@@ -19,3 +20,17 @@ fn grouped(s: String) -> HashMap<char, usize> {
     }
     stats
 }
+
+// a really nice solution from https://exercism.org/tracks/rust/exercises/anagram/solutions/itkovian
+// pub fn anagrams_for<'a>(target: &str, inputs: & [&'a str]) -> HashSet<&'a str> {
+//     fn sort_string(s: &str) -> Vec<char> {
+//         let mut target_chars: Vec<char> = s.to_lowercase().chars().collect();
+//         target_chars.sort();
+//         return target_chars;
+//     }
+//     let target_sorted = sort_string(&target);
+//     inputs.iter().cloned()
+//         .filter(|s| s.to_lowercase() != target.to_lowercase())
+//         .filter(|s| sort_string(s) == target_sorted)
+//         .collect()
+// }
