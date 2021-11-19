4a3474638e11b06beb3a617e40c76f383b83bda6
Gerd Reiss <gerd@reiss.pro>
2021-11-19 18:57:30 +0100

    House

---
+++ scala/house/src/main/scala/House.scala
@@ -0,0 +1,27 @@
+object House {
+
+  val clauses = List(
+    "the malt that lay in ",
+    "the rat that ate ",
+    "the cat that killed ",
+    "the dog that worried ",
+    "the cow with the crumpled horn that tossed ",
+    "the maiden all forlorn that milked ",
+    "the man all tattered and torn that kissed ",
+    "the priest all shaven and shorn that married ",
+    "the rooster that crowed in the morn that woke ",
+    "the farmer sowing his corn that kept ",
+    "the horse and the hound and the horn that belonged to "
+  )
+
+  def recite(x: Int, y: Int): String =
+    (x to y).map(withClauses).mkString + "\n"
+
+  def withClauses(x: Int): String =
+    "This is " + insertClauses(x - 1) + "the house that Jack built.\n"
+
+  def insertClauses(x: Int): String =
+    if (x <= 0) ""
+    else clauses(x - 1) + insertClauses(x - 1)
+
+}
